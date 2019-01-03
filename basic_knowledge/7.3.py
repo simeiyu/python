13baa31082f0cb149b7db2abed3985a0b0b3ca85
qiushui.smy <qiushui.smy@xuelangyun.com>
2019-01-03 17:09:33 +0800

    python test - 从入门到实践

--- basic_knowledge/7.3.py
+++ basic_knowledge/7.3.py
@@ -24,3 +24,29 @@ while unconfirmed_user:
 # 显示所有已验证的用户
 for user in confirmed_user:
     print(user.title())
+
+'''
+7.3.2 删除包含特定值的所有列表元素
+'''
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print('pets -->' + str(pets))
+while 'cat' in pets:
+    pets.remove('cat')
+
+print('after remove cat -->', str(pets))
+
+'''
+7.3.3 使用用户输入来填充字典
+'''
+responses = {}
+polling_active = True
+while polling_active:
+    name = input('What is your name?')
+    response = input('Which mountain would you like to climb someday?')
+    responses[name] = response
+    repeat = input('Would you like to let another person respond? (yes/ no)')
+    if repeat == 'no':
+        polling_active = False
+print('\n---- Poll Results ----')
+for name, response in responses.items():
+    print(name +' would like to climb ' + response + '.')
